a1fa1cbf5370a8cd22c6261f16dd2db574668f2c
Abdulrhman <abdomostafa9631@gmail.com>
2025-02-27 21:18:04 +0200

    repo faliure case

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/errors/faliures.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchFeatureBooks();
-  fetchBestSellerBooks();
+  Future<Either<Faliures, List<BookModel>>> fetchFeatureBooks();
+  Future<Either<Faliures, List<BookModel>>> fetchBestSellerBooks();
 }
